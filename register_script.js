027f8b67727b9d6fd78d7f504255ffb547db549c
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2019-11-29 10:10:51 -0500

    Initial commit
    Initial version of Register page

--- register_script.js
+++ register_script.js
@@ -1,11 +1,12 @@
 var url = "";
 document.addEventListener("DOMContentLoaded", bindButtons);
+document.addEventListener("DOMContentLoaded", bindPasswordField);
+document.addEventListener("DOMContentLoaded", bindConfirmPasswordField);
 
 function doPasswordsMatch() {
     var password = document.getElementById("inputPassword").value;
     var confPassword = document.getElementById("inputConfirmPassword").value;
     if (password === confPassword) {
-        alert("The passwords match!");
         return true;
     }
     return false;
@@ -20,9 +21,59 @@ function bindButtons() {
             payload.username = document.getElementById("inputUsername").value;
             payload.email = document.getElementById("inputEmail").value;
             payload.password = document.getElementById("inputPassword").value;
-        } else {
-            alert("Passwords don't match");
+        
+            req.open("POST", url, true);
+            req.setRequestHeader("Content-Type", "application/json");
+            req.addEventListener("load", function() {
+                if (req.status >= 200 && req.status < 400) {
+                    window.location.href="./login.html";
+                }
+            });
+            req.send(JSON.stringify(payload)); 
         }
         event.preventDefault();
+    });
+}
+
+function bindUsernameField() {
+    document.getElementById("inputUsername").addEventListener("input", function(event) {
+        var username = document.getElementById("inputUsername").value;
+        if (username === "") {
+            document.getElementById("inputUsername").classList.remove("is-invalid");
+        } else {
+            document.getElementById("inputUsername").classList.add("is-valid");
+        }
+    });
+}
+
+function bindPasswordField() {
+    document.getElementById("inputPassword").addEventListener("input", function(event) {
+        var passwordInput = document.getElementById("inputPassword").value;
+        var confirmPasswordInput = document.getElementById("inputConfirmPassword").value;
+        if (passwordInput === "" && confirmPasswordInput === "") {
+            document.getElementById("inputConfirmPassword").classList.remove("is-invalid");
+            document.getElementById("inputConfirmPassword").classList.remove("is-valid"); 
+        } else if (doPasswordsMatch()) {
+            document.getElementById("inputConfirmPassword").classList.remove("is-invalid");
+            document.getElementById("inputConfirmPassword").classList.add("is-valid");
+        } else {
+            document.getElementById("inputConfirmPassword").classList.add("is-invalid");
+        }
+    });
+}
+
+function bindConfirmPasswordField() {
+    document.getElementById("inputConfirmPassword").addEventListener("input", function(event) {
+        var passwordInput = document.getElementById("inputPassword").value;
+        var confirmPasswordInput = document.getElementById("inputConfirmPassword").value;
+        if (passwordInput === "" && confirmPasswordInput === "") {
+            document.getElementById("inputConfirmPassword").classList.remove("is-invalid");
+            document.getElementById("inputConfirmPassword").classList.remove("is-valid");
+        } else if (doPasswordsMatch()) {
+            document.getElementById("inputConfirmPassword").classList.remove("is-invalid");
+            document.getElementById("inputConfirmPassword").classList.add("is-valid");
+        } else {
+            document.getElementById("inputConfirmPassword").classList.add("is-invalid");
+        }
     });
 }
