03e3865e135ba784b1c5981321aac7c3cf40ad0b
MichaelJereza <mdj1999@gmail.com>
2019-11-28 16:54:56 -0800

    Server presents Login page

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ var express = require('express');
 
 var app = express();
 
+// Serve all resources in public
+app.use(express.static('public'));
+
+// Present html 
 app.get('/', function(req, res, next){
-    res.status(200).sendFile(__dirname + '/login.html');
+    res.status(200).sendFile(__dirname + '/public/login.html');
 });
 
+// Listen on port 3000
 app.listen(3000, function(error){
     if(error){
         throw error;
